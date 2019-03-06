dd41b95f9cc9cffe72e5ed5f98276fbbf6fad260
Manuel Zarat <manuel@zarat.ml>
2019-03-07 00:44:30 +0100

    Add files via upload

---
+++ quads.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+public class Circle {
+    public float x; 
+    public float y; 
+    public float r;
+}
+
+public class Quad {
+    public float x; 
+    public float y; 
+    public float w; 
+    public float h;
+    public int xdir;
+    public int ydir;
+    public string color;
+}
+
+public partial class AnimForm : Form {
+
+    private Timer timer = new Timer();
+    private float elapsedTime;
+    
+    public Graphics formGraphics;
+    public SolidBrush myBrush;
+    public Pen myPen;
+    
+    public Circle circle;
+    public IDictionary<int, Circle> circleDict = new Dictionary<int, Circle>();
+    
+    public Quad quad;
+    public IDictionary<int, Quad> quadDict = new Dictionary<int, Quad>();
+    public int i, j;
+
+    public AnimForm() {
+        
+        this.ClientSize = new System.Drawing.Size(480, 320);
+        formGraphics = this.CreateGraphics();
+        myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+        myPen = new Pen(myBrush);
+        
+        i = 0;
+        j = 0;
+
+        
+        quad = new Quad();
+        quad.x = 10.0f;
+        quad.y = 20.0f;
+        quad.w = 50.0f;
+        quad.h = 50.0f;
+        quad.xdir = 1;
+        quad.ydir = 1;
+        quad.color = "white";
+        quadDict.Add(0, quad);
+        
+        quad = new Quad();
+        quad.x = 180.0f;
+        quad.y = 180.0f;
+        quad.w = 50.0f;
+        quad.h = 50.0f;
+        quad.xdir = 0;
+        quad.ydir = 1;
+        quad.color = "white";
+        quadDict.Add(1, quad);
+        
+        quad = new Quad();
+        quad.x = 220.0f;
+        quad.y = 240.0f;
+        quad.w = 50.0f;
+        quad.h = 50.0f;
+        quad.xdir = 1;
+        quad.ydir = 0;
+        quad.color = "white";
+        quadDict.Add(2, quad);
+        
+        quad = new Quad();
+        quad.x = 70.0f;
+        quad.y = 80.0f;
+        quad.w = 30.0f;
+        quad.h = 30.0f;
+        quad.xdir = 0;
+        quad.ydir = 1;
+        quad.color = "white";
+        quadDict.Add(3, quad);
+        
+        quad = new Quad();
+        quad.x = 90.0f;
+        quad.y = 100.0f;
+        quad.w = 20.0f;
+        quad.h = 20.0f;
+        quad.xdir = 0;
+        quad.ydir = 0;
+        quad.color = "white";
+        quadDict.Add(4, quad);
+               
+        timer.Enabled = true;
+        timer.Tick += DancingBalls;
+        timer.Interval = 10;
+        timer.Start();
+        
+    }
+
+    private void DancingBalls( object sender, System.EventArgs e ) {
+        
+            //ticks++;
+            
+            formGraphics.Clear(Color.Black);
+            
+            elapsedTime = 2.5f;
+            
+            while(i<quadDict.Count) {
+                
+                /* Move everything */
+                if(quadDict[i].xdir == 1) quadDict[i].x += elapsedTime; 
+                if(quadDict[i].xdir == 0) quadDict[i].x -= elapsedTime; 
+                if(quadDict[i].ydir == 1) quadDict[i].y += elapsedTime; 
+                if(quadDict[i].ydir == 0) quadDict[i].y -= elapsedTime;
+                
+                /* Out of view detection */
+                if(quadDict[i].xdir == 1) {                    
+                    if(quadDict[i].x+quadDict[i].w >= 480.0) quadDict[i].xdir = 0;                   
+                } else if(quadDict[i].xdir == 0) {                    
+                    if(quadDict[i].x <= 1.0) quadDict[i].xdir = 1;                 
+                }            
+                if(quadDict[i].ydir == 1) {                    
+                    if(quadDict[i].y+quadDict[i].h >= 320.0) quadDict[i].ydir = 0;               
+                } else if(quadDict[i].ydir == 0) {                    
+                    if(quadDict[i].y <= 1.0) quadDict[i].ydir = 1;                
+                }
+            
+                /* Collision detection */
+                while(j<quadDict.Count) {                   
+                    if(j==i) { j++; continue; }                    
+                    if( 
+                        (quadDict[i].x < quadDict[j].x + quadDict[j].w && quadDict[i].x + quadDict[i].w > quadDict[j].x ) 
+                        && 
+                        (quadDict[i].y < quadDict[j].y + quadDict[j].h && quadDict[i].y + quadDict[i].h > quadDict[j].y ) 
+                    ) {
+                        quadDict[i].color = quadDict[j].color = "red";
+                    }  
+                    j++;                    
+                }
+                j = 0;
+                
+                if(quadDict[i].color == "red") formGraphics.FillRectangle(Brushes.Red, quadDict[i].x, quadDict[i].y, quadDict[i].w, quadDict[i].h);
+                else formGraphics.FillRectangle(Brushes.White, quadDict[i].x, quadDict[i].y, quadDict[i].w, quadDict[i].h);
+                
+                quadDict[i].color = "white";
+                
+                i++;
+                 
+            }        
+            i=0;
+            
+    }
+
+    [System.STAThread]
+    public static void Main() {
+        Application.EnableVisualStyles();
+        Application.Run( new AnimForm() );
+    }
+}
---
+++ triangles.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Triangle {
+    public float x1, x2, x3; 
+    public float y1, y2, y3; 
+    public float angle;
+}
+
+public partial class AnimForm : Form {
+
+    private Timer timer = new Timer();
+    private float elapsedTime;
+    
+    public Graphics formGraphics;
+    public SolidBrush myBrush;
+    public Pen myPen;
+    
+    public Triangle tri;
+    public IDictionary<int, Triangle> triDict = new Dictionary<int, Triangle>();
+    public int i, j;
+
+    public AnimForm() {
+        
+        this.ClientSize = new System.Drawing.Size(480, 320);
+        formGraphics = this.CreateGraphics();
+        myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+        myPen = new Pen(myBrush);
+        
+        i = 0;
+        j = 0;
+
+        
+        tri = new Triangle();
+        tri.x1 = 50.0f;
+        tri.x2 = 80.0f;
+        tri.x3 = 100.0f;
+        tri.y1 = 100.0f;
+        tri.y2 = 50.0f;
+        tri.y3 = 80.0f;
+        tri.angle = 2.0f;
+        triDict.Add(0, tri);
+        
+        tri = new Triangle();
+        tri.x1 = 80.0f;
+        tri.x2 = 110.0f;
+        tri.x3 = 130.0f;
+        tri.y1 = 130.0f;
+        tri.y2 = 80.0f;
+        tri.y3 = 110.0f;
+        tri.angle = 2.0f;
+        triDict.Add(1, tri);
+        
+        tri = new Triangle();
+        tri.x1 = 110.0f;
+        tri.x2 = 140.0f;
+        tri.x3 = 160.0f;
+        tri.y1 = 160.0f;
+        tri.y2 = 110.0f;
+        tri.y3 = 140.0f;
+        tri.angle = 2.0f;
+        triDict.Add(2, tri);
+               
+        timer.Enabled = true;
+        timer.Tick += DancingBalls;
+        timer.Interval = 10;
+        timer.Start();
+        
+    }
+
+    private void rotate(int i) {        
+        Triangle tri = triDict[i];        
+        double a1, b1, a2, b2, a3, b3;        
+        float p = tri.x1;
+        float q = tri.y1;         
+        float angle = (tri.angle * 3.14f) / 180; // Deg 2 Rad       
+        a1 = p + (tri.x1-p) * Math.Cos(angle)-(tri.y1-q) * Math.Sin(angle);
+        b1 = q + (tri.x1-p) * Math.Sin(angle)+(tri.y1-q) * Math.Cos(angle);
+        a2 = p + (tri.x2-p) * Math.Cos(angle)-(tri.y2-q) * Math.Sin(angle);
+        b2 = q + (tri.x2-p) * Math.Sin(angle)+(tri.y2-q) * Math.Cos(angle);
+        a3 = p + (tri.x3-p) * Math.Cos(angle)-(tri.y3-q) * Math.Sin(angle);
+        b3 = q + (tri.x3-p) * Math.Sin(angle)+(tri.y3-q) * Math.Cos(angle);        
+        tri.x1 = (float)a1;
+        tri.y1 = (float)b1;        
+        tri.x2 = (float)a2;
+        tri.y2 = (float)b2;        
+        tri.x3 = (float)a3;
+        tri.y3 = (float)b3;       
+        triDict[i] = tri;    
+    }
+
+    private void DancingBalls( object sender, System.EventArgs e ) {           
+            formGraphics.Clear(Color.Black);            
+            elapsedTime = 0.01f;           
+            while(i<triDict.Count) {    
+                rotate(i);                
+                PointF[] points = { 
+                    new PointF(triDict[i].x1, triDict[i].y1), 
+                    new PointF(triDict[i].x2, triDict[i].y2), 
+                    new PointF(triDict[i].x3, triDict[i].y3) 
+                };                
+                formGraphics.DrawPolygon(new Pen(Color.Red), points);
+                i++;                 
+            }        
+            i=0;           
+    }
+
+    [System.STAThread]
+    public static void Main() {
+        Application.EnableVisualStyles();
+        Application.Run( new AnimForm() );
+    }
+}
